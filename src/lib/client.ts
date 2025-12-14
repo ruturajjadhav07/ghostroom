f2a9894b08350bc40b4f0ec41dee3d802d88c29b
“[github]” <ruturajjadhav122@gmail.com>
2025-12-14 10:09:07 +0530

    updated code for path url

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -2,5 +2,9 @@ import { treaty } from '@elysiajs/eden'
 import type { App } from '../app/api/[[...slugs]]/route'
 
 // this require .api to enter /api prefix
-export const client = treaty<App>('localhost:3000').api
+// export const client = treaty<App>('localhost:3000').api
 
+
+const BASE_URL = 'localhost:3000'
+
+export const client = treaty<App>(BASE_URL).api
