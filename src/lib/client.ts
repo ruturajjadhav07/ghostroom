4630eda5669e22eb3eadb09396c57ff49251bc7b
“[github]” <ruturajjadhav122@gmail.com>
2025-12-24 18:13:38 +0530

    replace hardcoded localhost with relative API base

--- src/lib/client.ts
+++ src/lib/client.ts
@@ -4,10 +4,4 @@ import type { App } from '../app/api/[[...slugs]]/route'
 // this require .api to enter /api prefix
 // export const client = treaty<App>('localhost:3000').api
 
-
-const BASE_URL =
-  typeof window === 'undefined'
-    ? 'http://localhost:3000'
-    : ''
-
-export const client = treaty<App>(BASE_URL).api
+export const client = treaty<App>("/").api
